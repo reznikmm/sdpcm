0180a4707e6326bc8d2f8365ac40165f7326fbb1
Maxim Reznik <reznikmm@gmail.com>
2026-01-10 16:02:35 +0200

    Add license files

--- source/sdpcm.ads
+++ source/sdpcm.ads
@@ -1,3 +1,8 @@
-package Sdpcm is
+--  SPDX-FileCopyrightText: 2026 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: MIT
+----------------------------------------------------------------
 
+package Sdpcm is
+   pragma Pure;
 end Sdpcm;
